1f307d19965abf66e9c04df03c0b494298a190ef
treeshay <shayleighton24@gmail.com>
2025-09-16 13:40:12 +0100

    Moved functions out of the main() function.
    Moved all the functions to be outside the main function and passed in the car_wash parameter

--- car_wash.py
+++ car_wash.py
@@ -73,130 +73,124 @@ class CarWash:
                 self.total_revenue += price
                 print("Washed")
 
+def add_new_car(car_wash):
+    #Gets user input relating to all car attributes before making a new car
+    owner = empty_validation("Enter owners name: ")
+    reg = empty_validation("Enter registration number: ")
+    contact_num = empty_validation("Enter owner's contact number: ")
+    make = empty_validation("Enter the make of the car: ")
+    model = empty_validation("Enter the model of the car: ")
+    year = year_validation("Enter the year of the car: ")
+    size = size_validation("Enter the size of the car (Small/Large): ")
+    new_car = Car(owner, reg, contact_num, make, model, year, size)
+    car_wash.add_car(new_car)
+    print("Car added.")
+    save_car(new_car)
+
+def save_car(car):
+    with open("cars.txt", "a") as file:
+        file.write(f"{car.owner},{car.reg},{car.contact_num},"
+                    f"{car.make},{car.model},{car.year},{car.size}\n")
+        print("car has been saved to cars.txt")
+
+def load_cars(car_wash):
+    with open("cars.txt", "r") as file:
+        data = file.readlines()
+        for line in data:
+            if line:
+                fields = [field.strip() for field in line.split(",")]
+                if len(fields) == 7:
+                    fields[5] = int(fields[5]) #Load year in as int
+                    car = Car(*fields)
+                    car_wash.cars.append(car)
+                else:
+                    print(f"Skipping line: {line}")
+
+def view_dirty_cars(car_wash):
+    dirty_cars = car_wash.dirty_cars()
+    for car in dirty_cars:
+        print(car)
+        
+def create_car_wash():
+    #Creates an instance of a car wash.
+    name = input("Enter your car wash name: ")
+    small_price = int(input("Enter price of a small wash: "))
+    large_price = int(input("Enter price of large wash: "))
+    return CarWash(name, small_price, large_price)
+
+def empty_validation(prompt):
+    #Ensures user input is not left empty, will loop until field is not empty
+    while True:
+        answer = input(prompt).strip()
+        if answer:
+            return answer
+        else:
+            print("Can not be empty. Please try again")
+
+def year_validation(prompt):
+    #Checks a year making sure it cant be before 1884 or after current year
+    while True:
+        current_year = datetime.now().year
+        oldest_year = 1884 #Oldest running car
+        year = int(empty_validation(prompt)) #Ensures year is not empty
+        if year > current_year:
+            print(f"Year must be before {current_year}. You entered {year}. ")
+        elif year < oldest_year:
+            print(f"Year must be after {current_year}. You entered {year}. ")
+        elif year:
+            return year
+        else:
+            print(f"Please enter a valid year. You entered {year}")
+
+def size_validation(prompt):
+    #Ensures a user only enters valid sizes
+    while True:
+        size = empty_validation(prompt).lower()
+        if size in ["small", "large"]:
+            return size
+        else:
+            print("Size must be 'small' or 'large'")
+            print(f"You entered {size}")
+
 
 def main():
-
-    def save_car(car):
-        with open("cars.txt", "a") as file:
-            file.write(f"{car.owner},{car.reg},{car.contact_num},"
-                        f"{car.make},{car.model},{car.year},{car.size} \n")
-            print("car has been saved to cars.txt")
-
-    def load_cars():
-        with open("cars.txt", "r") as file:
-            data = file.readlines()
-            for line in data:
-                if line:
-                    fields = [field.strip() for field in line.split(",")]
-                    if len(fields) == 7:
-                        fields[5] = int(fields[5]) #Load year in as int
-                        car = Car(*fields)
-                        car_wash.cars.append(car)
-                    else:
-                        print(f"Skipping line: {line}")
-
-    def create_car_wash():
-        #Creates an instance of a car wash.
-        name = input("Enter your car wash name: ")
-        small_price = int(input("Enter price of a small wash: "))
-        large_price = int(input("Enter price of large wash: "))
-        return CarWash(name, small_price, large_price)
-
-    def empty_validation(prompt):
-        #Ensures user input is not left empty, will loop until field is not empty
-        while True:
-            answer = input(prompt).strip()
-            if answer:
-                return answer
-            else:
-                print("Can not be empty. Please try again")
-
-    def year_validation(prompt):
-        #Checks a year making sure it cant be before 1884 or after current year
-        while True:
-            current_year = datetime.now().year
-            oldest_year = 1884 #Oldest running car
-            year = int(empty_validation(prompt)) #Ensures year is not empty
-            if year > current_year:
-                print(f"Year must be before {current_year}. You entered {year}. ")
-            elif year < oldest_year:
-                print(f"Year must be after {current_year}. You entered {year}. ")
-            elif year:
-                return year
-            else:
-                print(f"Please enter a valid year. You entered {year}")
-
-    def size_validation(prompt):
-        #Ensures a user only enters valid sizes
-        while True:
-            size = empty_validation(prompt).lower()
-            if size in ["small", "large"]:
-                return size
-            else:
-                print("Size must be 'small' or 'large'")
-                print(f"You entered {size}")
-
-    def add_new_car():
-        #Gets user input relating to all car attributes before making a new car
-        owner = empty_validation("Enter owners name: ")
-        reg = empty_validation("Enter registration number: ")
-        contact_num = empty_validation("Enter owner's contact number: ")
-        make = empty_validation("Enter the make of the car: ")
-        model = empty_validation("Enter the model of the car: ")
-        year = year_validation("Enter the year of the car: ")
-        size = size_validation("Enter the size of the car (Small/Large): ")
-        new_car = Car(owner, reg, contact_num, make, model, year, size)
-        car_wash.add_car(new_car)
-        print("Car added.")
-        save_car(new_car)
-
-    def view_dirty_cars():
-        dirty_cars = car_wash.dirty_cars()
-        for car in dirty_cars:
-            print(car)
-
     car_wash = create_car_wash()
-    load_cars()
+    load_cars(car_wash)
 
     while True:
-            print(f'''CAR WASH
+        print(f'''CAR WASH
 1) Add car
 2) View dirty cars
 3) Wash car
 4) View revenue
 5) Exit''')
 
+        user_choice = int(input("Please select an option (1/5): "))
 
-            user_choice = int(input("Please select an option (1/5): "))
+        #ADD CAR
+        if user_choice == 1:
+            add_new_car(car_wash)
 
-            #ADD CAR
-            if user_choice == 1:
-                add_new_car()
+        if user_choice == 2:
+            view_dirty_cars(car_wash)
 
+        if user_choice == 3:
+            car_reg = input("Search registration of car to wash: ")
+            found = False
+            for car in car_wash.cars:
+                if car_reg.lower() == car.reg.lower():
+                    car_wash.wash(car)
+                    found = True
+                    break
+            if not found:
+                print(f"{car_reg} is not valid.")
 
-            if user_choice == 2:
-                view_dirty_cars()
+        if user_choice == 4:
+            print(car_wash.total_revenue)
 
-            if user_choice == 3:
-                car_reg = input("Search registration of car to wash: ")
-                found = False
-                for car in car_wash.cars:
-                    if car_reg.lower() == car.reg.lower():
-                        car_wash.wash(car)
-                        found = True
-                        break
-                if not found:
-                    print(f"{car_reg} is not valid.")
-
-
-            if user_choice == 4:
-                print(car_wash.total_revenue)
-
-
-
+        if user_choice == 5:
+            print("Goodbye!")
+            break
 
 if __name__ == '__main__':
     main()
-
-
-
